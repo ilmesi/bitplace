9d86107b3375158eec185fff516cf525a87e133e
ignacio <ignacio@bitpagos.co>
2014-03-01 16:31:27 -0300

    product templates

--- bitplace/market/urls.py
+++ bitplace/market/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^product/$', 'market.views.product', name='product'),
+    url(r'^product/(?P<id>\d)/$', 'market.views.product', name='product'),
     url(r'^callback', 'market.views.callback', name='callback'),
 )
--- bitplace/market/views.py
+++ bitplace/market/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, RequestContext
 
 
 def index(request):
     pass
+
+def product(request, id):
+    return render_to_response('market/product.html', RequestContext(request))
 
 def callback(request):
     pass
