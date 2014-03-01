2b3d74beb0be029939d0f185a9e3a057be7e6087
brian <brian@brian-pc.(none)>
2014-03-01 17:16:45 -0300

    fixing small bugs in pay and callback

--- bitplace/market/urls.py
+++ bitplace/market/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^product/(?P<id>\d)/$', 'market.views.product', name='product'),
     url(r'^callback', 'market.views.callback', name='callback'),
+    url(r'^pay', 'market.views.pay', name='pay'),
 )
